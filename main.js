df4eb95551655e1790d222f3023d142eaf2107aa
Mattia <mattia@scruzzi.com>
2026-03-30 12:31:42 +0200

    feat: add much more variety to quotes

--- main.js
+++ main.js
@@ -13,7 +13,31 @@
       "Qualitäts-Bauch.",
       "Legendär!",
       "Bauch-tastisch!",
-      "Einfach nur WOW."
+      "Einfach nur WOW.",
+      "Ein makelloser Waschbärbauch.",
+      "Absolute Wohlfühlzone.",
+      "Dieser Bauch verdient einen Orden.",
+      "Ein Bauch wie ein Kunstwerk.",
+      "Sooo weich und flauschig!",
+      "Perfekte Resonanz beim Streicheln.",
+      "Erstklassiges Streichel-Feedback.",
+      "Jeder Streicherler ein Genuss.",
+      "Da steckt viel Liebe (und gutes Essen) drin.",
+      "Die reinste Entspannung für die Hände.",
+      "Ein meisterhaft gepflegtes Bäuchlein.",
+      "Das ist Premium-Qualität.",
+      "Spürst du diese Aura?",
+      "Ein Bauch für die Geschichtsbücher.",
+      "Da kann man direkt neidisch werden.",
+      "Einfach himmlisch.",
+      "Sanfter als ein Wolkenschloss.",
+      "Majestätische Formgebung.",
+      "Kein Sixpack, aber ein ganzes Fass voll Glück.",
+      "Das nennt man einen echten Wohlstandsbauch!",
+      "Streicheleinheiten erfolgreich geloggt.",
+      "Mehr davon, bitte!",
+      "Der Bauch freut sich sichtlich.",
+      "Ein wahrer Traum!"
     ];
 
     let count = parseInt(localStorage.getItem('niceCount')) || 0;

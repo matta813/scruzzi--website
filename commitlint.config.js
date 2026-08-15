cb3f393f7919a294f0b743d466f39eb1ee0337f5
Mattia <scruzzimattia@gmail.com>
2026-08-15 23:32:13 +0000

    ci(commitlint): allow generated dependency links

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,5 @@
-module.exports = { extends: ["@commitlint/config-conventional"] };
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  // Dependabot release links can exceed the conventional 100-character body limit.
+  rules: { "body-max-line-length": [0] },
+};
